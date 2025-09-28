bda4dc2367f7ae577e795f07c66ad0984376f42b
mukesh <>
2025-09-28 11:17:57 +0530

    Added headless Chrome support for CI/CD and improved browser setup

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -3,6 +3,7 @@ package base;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.*;
 import utils.ConfigReader;
 
@@ -15,7 +16,20 @@ public class BaseTest {
     public void setUp() {
         ConfigReader.loadConfig();
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+
+        ChromeOptions options = new ChromeOptions();
+
+        // Check if running in CI/CD environment or headless mode
+        String headless = System.getProperty("headless");
+        if ("true".equals(headless) || System.getenv("CI") != null) {
+            options.addArguments("--headless");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--disable-gpu");
+            options.addArguments("--window-size=1920,1080");
+        }
+
+        driver = new ChromeDriver(options);
 
         driver.manage().window().maximize();
         //driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
